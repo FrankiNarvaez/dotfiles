0221ab8e5a13183e4f3b6716e11eb394069cd703
FrankiNarvaez <frannki03@gmail.com>
2025-02-22 21:02:13 -0500

    feat: fzf-lua pluging

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,6 +20,14 @@ vim.g.maplocalleader = "\\"
 
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    opts = {}
+  },
 }
 local opts = {}
 
@@ -28,3 +36,8 @@ require("lazy").setup(plugins, opts)
 -- Setup colorscheme
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+-- Setup fzf-lua
+local fzf_lua = require("fzf-lua")
+vim.keymap.set('n', '<leader><leader>', fzf_lua.files, { noremap = true, silent = true, desc = 'Search...' })
+vim.keymap.set('n', '<leader>sg', fzf_lua.live_grep, { noremap = true, silent = true, desc = 'Search...' })
