ff420c1396debdd3de9f2bba7eaf6d89cd99278d
FrankiNarvaez <frannki03@gmail.com>
2025-02-23 10:52:05 -0500

    refactor: remove config function

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require("telescope.builtin")
-    end
+    dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
