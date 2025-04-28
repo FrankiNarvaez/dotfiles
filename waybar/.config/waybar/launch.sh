1dc67ad5ee197ec19359acffc0b07da3f1937e7b
FrankiNarvaez <frannki03@gmail.com>
2025-04-27 21:18:51 -0500

    added waybar

---
+++ waybar/.config/waybar/launch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#  ____  _             _    __        __          _                 
+# / ___|| |_ __ _ _ __| |_  \ \      / /_ _ _   _| |__   __ _ _ __  
+# \___ \| __/ _` | '__| __|  \ \ /\ / / _` | | | | '_ \ / _` | '__| 
+#  ___) | || (_| | |  | |_    \ V  V / (_| | |_| | |_) | (_| | |    
+# |____/ \__\__,_|_|   \__|    \_/\_/ \__,_|\__, |_.__/ \__,_|_|    
+#                                           |___/                   
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+# Check if waybar-disabled file exists
+if [ -f $HOME/.cache/waybar-disabled ] ;then 
+    killall waybar
+    pkill waybar
+    exit 1 
+fi
+
+# ----------------------------------------------------- 
+# Quit all running waybar instances
+# ----------------------------------------------------- 
+killall waybar
+pkill waybar
+sleep 0.2
+
+# ----------------------------------------------------- 
+# Default theme: /THEMEFOLDER;/VARIATION
+# ----------------------------------------------------- 
+themestyle="/ml4w-blur"
+
+# ----------------------------------------------------- 
+# Get current theme information from .cache/.themestyle.sh
+# ----------------------------------------------------- 
+if [ -f ~/.cache/.themestyle.sh ]; then
+    themestyle=$(cat ~/.cache/.themestyle.sh)
+else
+    touch ~/.cache/.themestyle.sh
+    echo "$themestyle" > ~/.cache/.themestyle.sh
+fi
+
+# ----------------------------------------------------- 
+# Loading the configuration
+# ----------------------------------------------------- 
+waybar -c ~/.config/waybar/themes${themestyle}/config -s ~/.config/waybar/themes${themestyle}/style.css &
