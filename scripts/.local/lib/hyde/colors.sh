ed3c0b659bd17076ae52158d5ddd289954b0bbf6
FrankiNarvaez <frannki03@gmail.com>
2025-07-06 19:18:34 -0500

    update spotify colors

--- scripts/.local/lib/hyde/colors.sh
+++ scripts/.local/lib/hyde/colors.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-
 SPICETIFY_THEMES_DIR="$HOME/.config/spicetify/Themes"
-THEME="text"
-COLOR_SCHEME="Kanagawa"
+THEME="Ziro"
+COLOR_SCHEME="Wal"
 
 # Read pywal colors
 source "$HOME/.cache/wal/colors.sh"
 
-# Create color with color.ini
+# Create color.ini with pywal colors mapped to new properties
 cat >"$SPICETIFY_THEMES_DIR/$THEME/color.ini" <<EOF
-[Kanagawa]
-accent             = ${color6#\#}
-accent-active      = ${color14#\#}
-accent-inactive    = ${background#\#}
-banner             = ${color6#\#}
-border-active      = ${color6#\#}
-border-inactive    = ${color8#\#}
-header             = ${color8#\#}
-highlight          = ${color0#\#}
-main               = ${background#\#}
-notification       = ${color4#\#}
-notification-error = ${color1#\#}
-subtext            = ${color7#\#}
+[Wal]
 text               = ${foreground#\#}
+subtext            = ${color7#\#}
+main               = ${background#\#}
+sidebar            = ${background#\#}
+player             = ${background#\#}
+card               = ${color0#\#}
+shadow             = ${color0#\#}
+selected-row       = ${color6#\#}
+button             = ${color6#\#}
+button-active      = ${color14#\#}
+button-disabled    = ${color8#\#}
+tab-active         = ${color6#\#}
+notification       = ${color0#\#}
+notification-error = ${color1#\#}
+misc               = ${background#\#}
 EOF
 
 # Apply spicetify theme
